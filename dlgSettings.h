0f55cccda70ad14d8cb234cbb02c50ed23decffb
Mirza Nakicevic <mirza@linux.org.ba>
2012-02-15 22:18:28 +0100

    licence header in files changed, added pcb files for hardware, Cadsoft Eagle 6 format

--- dlgSettings.h
+++ dlgSettings.h
@@ -1,18 +1,18 @@
 /*
  * This file is part of ART application.
  *
- * I/O Control Board is free software: you can redistribute it and/or modify
+ * ART is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- *  I/O Control Board is distributed in the hope that it will be useful,
+ *  ART is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with I/O Control Board. If not, see <http://www.gnu.org/licenses/>.
+ *  along with ART. If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef DLGSETTINGS_H
 #define	DLGSETTINGS_H
